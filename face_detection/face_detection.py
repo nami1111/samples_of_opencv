16d76e4dd19e4da1cc24f292c59fe254d47c8a4e
nami1111 <nkawa1111@gmail.com>
2019-03-21 21:05:24 +0900

    [clean] clean my heart...

--- face_detection/face_detection.py
+++ face_detection/face_detection.py
@@ -1,28 +1,25 @@
 import cv2
 from datetime import datetime
 
-DEVICE_ID = 0
-WINDOW_NAME = "opencv-face"
-MIN_SIZE = (70, 70)
+CASCADE_DIR = r'C:\Users\nkawa\AppData\Local\Programs\Python\Python37-32\Lib\site-packages\cv2\data'
+CASCADE_FILE = CASCADE_DIR + r'\haarcascade_frontalface_alt.xml'
+WINDOW_NAME = "face_detection"
 
-cascade_file = r'C:\Users\nkawa\AppData\Local\Programs\Python\Python37-32\Lib\site-packages\cv2\data\haarcascade_frontalface_alt.xml'
-cascade = cv2.CascadeClassifier(cascade_file)
+cascade = cv2.CascadeClassifier(CASCADE_FILE)
 
-camera = cv2.VideoCapture(DEVICE_ID)
+camera = cv2.VideoCapture(0)
 
 cv2.namedWindow(WINDOW_NAME)
 
 while True:
-    # Capture frame-by-frame
     _, img = camera.read()
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    face_list = cascade.detectMultiScale(img_gray, minSize=MIN_SIZE)
+    face_list = cascade.detectMultiScale(img_gray, minSize=(70, 70))
 
     for (x, y, w, h) in face_list:
         color = (0, 255, 0)
         pen_w = 3
-        cv2.rectangle(img, (x, y), (x+w, y+h),
-                      color, thickness=pen_w)
+        cv2.rectangle(img, (x, y), (x+w, y+h), color, thickness=pen_w)
 
     cv2.imshow(WINDOW_NAME, img)
 
